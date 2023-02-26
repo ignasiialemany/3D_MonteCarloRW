5ea68b458698e46584d090b1f65a1a8d6a108e0b
Ignasi <ignasialemanyjuv@gmail.com>
2023-02-26 20:24:33 +0100

    Added substrate

--- Substrate/substrate.h
+++ Substrate/substrate.h
@@ -5,12 +5,19 @@
 #ifndef INC_3DRANDOMWALK_SUBSTRATE_H
 #define INC_3DRANDOMWALK_SUBSTRATE_H
 
-
+#include "transform.h"
 
 class substrate {
 
-};
+public:
+    substrate()=default;
 
+
+
+private:
+
+
+};
 
 
 #endif //INC_3DRANDOMWALK_SUBSTRATE_H
