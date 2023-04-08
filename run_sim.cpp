415de6ea1928b534197c284f3055018e9cdf8d6d
Ignasi <ignasialemanyjuv@gmail.com>
2023-04-08 20:33:16 +0200

    run_sim for FE comparison

--- run_sim.cpp
+++ run_sim.cpp
@@ -11,7 +11,7 @@ int main()
    std::string parent_path_str = parent_path.string();
    // Get substring of parent_path_str subtracting the last 5 characters
    std::string grandparent_path_str = parent_path_str.substr(0, parent_path_str.size() - 5);
-   std::string file_path = "geometry_1.mat";
+   std::string file_path = "2d_geo_rw.mat";
    std::string seq_path = "sequence.yaml";
    std::string full_path = grandparent_path_str + file_path;
    std::string seq_full_path = grandparent_path_str + seq_path;
@@ -23,33 +23,40 @@ int main()
    auto data = utility_substrate::read_mat_file(full_path);
    substrate substrate(data.first, data.second);
    // Create sequence
-   sequence seq(seq_full_path);
-   seq.create();
+   sequence seq;
 
    Eigen::VectorXd voxel(6);
-   double buffer_zone = std::sqrt(6 * 2.5 * seq.dt.sum());
-   voxel << 0.0 - buffer_zone, 2800. + buffer_zone, 0.0 - buffer_zone, 2800. + buffer_zone, 0.0 - buffer_zone, 8000. + buffer_zone;
+   //double buffer_zone = std::sqrt(6 * 2.5 * seq.dt.sum());
+   voxel << 47. , 52., 42., 47., 50., 52.;
    substrate.setVoxel(voxel);
-   transform t(0.01, true, false);
-   t.set_block(495.4, 392.3, 126.6);
+   transform t(0., false, false);
+   t.set_block(109., 76., 126.6);
    substrate.setTransform(t);
 
    // Initialize simulation
    std::cout << "Initializing simulation" << std::endl;
+   std::vector<double> kappas{0.05};
 
-   std::vector<double> kappas{0,0.01,0.02,0.03,0.04,0.05};
+   Eigen::VectorXd durations(10000);
+   durations.setConstant(501./10000.);
+
+   //Create a VetorXd same length as durations with all elements equal to 1
+   Eigen::VectorXd ids = Eigen::VectorXd::Ones(durations.size());
+   seq.dt = durations;
+   seq.gG = ids;
+
 
    for (auto &kappa : kappas)
    {
       // Create simulation
       int seed = rand() % 100000 + 1;  // Generate a random number between 1 and 100000
-      simulation sim(10000, seed);
+      simulation sim(500000, seed);
       // Seed particles
       sim.seedParticlesInBox(substrate);
 
       // Write initial positions
-      std::string file_init_positions = "STEAM_seed_" + std::to_string(seed) + "_kappa_" + std::to_string(kappa) + "_fixed_step_init.csv";
-      sim.writeParticlesState(file_init_positions);
+      std::string file_init_positions = "RW_seed_" + std::to_string(seed) + "_kappa_" + std::to_string(kappa) + "_fixed_step_init.csv";
+      sim.writeParticlesState(file_init_positions,substrate);
 
       // Set parameters
       sim.params.cores = 14;
@@ -66,8 +73,8 @@ int main()
       std::cout << "Elapsed simulation time: " << elapsed.count() << " seconds" << std::endl;
 
       // Write final positions
-      std::string file_final_positions = "STEAM_seed_" + std::to_string(seed) + "_kappa_"  + std::to_string(kappa) + "_fixed_step_final.csv";
-      sim.writeParticlesState(file_final_positions);
+      std::string file_final_positions = "RW_seed_" + std::to_string(seed) + "_kappa_"  + std::to_string(kappa) + "_fixed_step_final.csv";
+      sim.writeParticlesState(file_final_positions,substrate);
    }
 
    return 0;
